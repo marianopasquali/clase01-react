1734d889605eef26348949f7a72b6cb373f0293c
marianopasquali <mariano_pasquali@hotmail.com>
2021-01-05 18:47:37 -0300

    se agrega link a github

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () =>{
             <footer className="footer">
                 <span className="owner">2021 Curso React - Mariano Pasquali</span>
                 <span className="icons">
-                    <a href="" className="icon github"><FontAwesomeIcon icon={faGithubSquare} /></a>
+                    <a href="https://github.com/marianopasquali/clase01-react" className="icon github"><FontAwesomeIcon icon={faGithubSquare} /></a>
                     <a href="https://www.linkedin.com/in/mariano-yael-pasquali-678615178/" className="icon linkedin"><FontAwesomeIcon icon={faLinkedin} /></a>
                 </span>
                 
